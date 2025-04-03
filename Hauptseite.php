7bd98c647f1877a044875108c7efb0fea861d191
PhilipWie_ <philip@wiemermail.de>
2025-04-03 17:03:31 +0200

    first commit

---
+++ Hauptseite.php
@@ -0,0 +1,76 @@
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="style.css" title="css" />
+
+    <title>Handy Laden</title>
+</head>
+
+<body>
+    <div class="obere_leiste">
+        <nav>
+            <img alt="logo"> |
+            <a href="Hauptseite.php">Alle Produkte</a> |
+            <a href="Bestellung.php">Deine Bestellungen</a> |
+            <a href="Support.html">Hilfe</a> |
+            <a href="Registrierung.php">Anmeldung / Registrierung</a> |
+        </nav>
+    </div>
+	<h1 class="header">Handy Laden</h1>
+    <div class="container">
+        
+
+        <?php
+        include("zugriff.inc");
+        $conn = mysqli_connect($host, $user, $password, $db) or die("conn dead");
+        $query = "Select * from produkte ";
+        $result = mysqli_query($conn, $query) or die("query dead");
+
+        while ($daten = mysqli_fetch_object($result)) {
+            $name = $daten->Pr_Name;
+            $preis = $daten->Pr_Preis;
+            $bst = $daten->Pr_Lagerbst;
+            $bild = $daten->Pr_bild;
+            $id = $daten->Pr_Nr;
+
+
+            echo "<div class='divProdukt'>";
+            echo "    <h2>$name</h2>";
+            echo "    <div class='divProdBild'>";
+            echo "        <img class='prod-img1' src='$bild'>";
+            echo "    </div>";
+            if ($bst >= 1) {
+                echo "<p class='font'>$preis € <strong>Lieferbar</strong> </p>";
+            } else {
+                echo "<p>$preis € <strong>Derzeit nicht Verfügbar</strong> </p>";
+            }
+            echo "<form action='Bestellung.php' method='Post'>";
+            echo "Menge: <select name='bst_menge'>";
+            for ($i = 1; $i <= $bst; $i++) {
+                echo "<option value='$i'>$i</option>";
+            }
+            echo "</select>";
+
+            echo "<br><br>
+    <button type='submit' name='subm_Bst' value='$id' >Bestellen</button>
+	</form>";
+
+            echo "</div>";
+        }
+        mysqli_close($conn);
+        ?>
+    </div>
+
+
+    <br>
+    <br>
+    <footer class="Footer">
+        <p>
+            &copy; Alle Rechte vorbehalten
+        </p>
+    </footer>
+</body>
+
+</html>
---
+++ KndRegist.php
@@ -0,0 +1,69 @@
+<?php
+session_start();
+  include('zugriff.inc');
+  $conn = mysqli_connect($host, $user, $password, $db) or die('Conn Dead');
+$regORan = $_POST['action'];
+$abme = $_POST['abmelden'];
+if($regORan == 'anmeldung'){
+    anmeldung($conn);
+}else if($regORan == 'registrierung'){
+    registrierung($conn);
+}else{
+    //abmeldung
+    session_destroy();
+    echo "Sie wurden abgemeldet";
+    echo "<form action='Registrierung.php'> <h2>Melden sie
+    sich hier an</h2>"; 
+}
+  mysqli_close($conn);
+
+function anmeldung($conn){
+
+    $email = $_POST['email'];
+    $passwort = $_POST['password'];
+    
+    $query = "Select K_Nr,K_Vorname from kunden where K_Mail = '$email' and K_Psw = '$passwort'";
+    $result = mysqli_query($conn, $query) or die('Query Dead');
+    $daten = mysqli_fetch_object($result);
+    $K_Nr = $daten->K_Nr;
+    $K_Vorname = $daten-> K_Vorname;
+    
+    if($K_Nr != null){
+        echo "Anmeldung erfolgreich";
+        $_SESSION['email'] = $email;
+        $_SESSION['name'] = $K_Vorname;
+        $_SESSION['K_Nr'] = $K_Nr;
+    }else{
+        echo "Anmeldung fehlgeschlagen";
+    }
+}
+function registrierung($conn){
+    $Vname = $_POST['Vorname'];
+    $Nname = $_POST['Nachname'];
+    $email = $_POST['email'];
+    $strasse = $_POST['Strasse'];
+    $Hnr = $_POST['Hnr'];
+    $PLZ = $_POST['PLZ'];
+    $Ort = $_POST['Ort'];
+    $passwort = $_POST['password'];
+    
+   
+    //neue Kundennummer generieren
+    $query = "Select Max(K_Nr) as max_kn from kunden";
+    $result = mysqli_query($conn, $query) or die('Query Dead');
+    $daten = mysqli_fetch_object($result);
+    $K_Nr = $daten->max_kn;
+    
+    $K_Nr = $K_Nr + 1;
+    $query = "Insert into kunden (K_Nr,K_Vorname, K_Nachname, K_Mail, K_Str, K_HNR, K_Pstl, K_Ort, K_Psw) values ($K_Nr,'$Vname', '$Nname', '$email', '$strasse', '$Hnr', '$PLZ', '$Ort', '$passwort')";
+    mysqli_query($conn, $query) or die('Query Dead');
+    
+    $query = "Select K_Nr from kunden";
+    $result = mysqli_query($conn, $query) or die('Query Dead');
+    Echo "Sie haben sich erfolgreich registriert";
+    Echo "<form action='Registrierung.php'> <h2>Melden sie sich hier an</h2>
+    <input type='submit' value='anmeldung' name='action'>";
+
+}
+
+?>
